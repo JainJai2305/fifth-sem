2b4bc6d097ffa85329083ed14bd5cf0e76c7b7a3
JainJai2305 <53936875+JainJai2305@users.noreply.github.com>
2020-12-30 15:33:09 +0530

    Update prog2.py

--- open end eval/prog2.py
+++ open end eval/prog2.py
@@ -1,14 +1,21 @@
+def uniq (i) :
+    ls=[]
+    a=1
+    while i!=0 :
+        j=i%10
+        if j in ls :
+            a=-1 
+            break
+        ls.append(j)
+        i=int(i/10)
+    return a
+
+
 print("enter the limits l and r")
 l=int(input())
 r=int(input())
 
 j=0
 for i in range (l,r+1) :
-    if i in range(1,100) and i%11 == 0  :
-        continue
-    if i in range(100,1000) and i%111 == 0  :
-        continue
-    if i in range(1000,10000) and i%1111 == 0  :
-        continue
+    if uniq(i) == -1 : continue
     print(i,end=" ")
-
